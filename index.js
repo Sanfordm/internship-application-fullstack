859448df2a64235f3272863ce102b9e93bda1ec0
Matt <sanfordm@southwestern.edu>
2020-04-17 19:25:13 -0500

    Basic requirements implemented.

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
+
 /**
- * Respond with hello worker text
+ * Responds with a random URL based on new request response.
  * @param {Request} request
  */
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+  var initial_response = await fetch('https://cfw-takehome.developers.workers.dev/api/variants')
+    .then(response => response.json())
+    .then(data => data.variants)
+    .catch(err => {
+      console.log('something went wrong')
+    })
+  var new_response = await fetch(randomChoice(initial_response))
+  return new HTMLRewriter().on('*', new ElementHandler()).transform(new_response)
+}
+
+/**
+ * Selects random choice between elements
+ * @param {array} arr
+ */
+function randomChoice(arr){
+  var index = Math.round(Math.random())
+  return arr[index]
+}
+
+class ElementHandler {
+  element(element) { }
+
+  comments(comment) { }
+
+  text(text) { }
 }
