d69ef3d8f3a855de781c7868c224f9fd09a46b8f
Matt <sanfordm@southwestern.edu>
2020-04-19 15:27:58 -0500

    Added HTMLrewriter definitions for EC.

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+var variant_1_count = 0
+var variant_2_count = 0
+var index = -1
+
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
@@ -14,7 +18,11 @@ async function handleRequest(request) {
       console.log('something went wrong')
     })
   var new_response = await fetch(randomChoice(initial_response))
-  return new HTMLRewriter().on('*', new ElementHandler()).transform(new_response)
+  return new HTMLRewriter()
+    .on('title', new ElementHandler())
+    .on('p#description', new ElementHandler())
+    .on('a#url', new ElementHandler())
+    .transform(new_response)
 }
 
 /**
@@ -22,14 +30,41 @@ async function handleRequest(request) {
  * @param {array} arr
  */
 function randomChoice(arr){
-  var index = Math.round(Math.random())
+  index = Math.round(Math.random())
+  if(index == 0){
+    variant_1_count++
+  }else{
+    variant_2_count++
+  }
   return arr[index]
 }
 
+/**
+ * Handles HTMLrewriter object definitions, manipulates specific tags.
+ */
 class ElementHandler {
-  element(element) { }
-
-  comments(comment) { }
-
-  text(text) { }
+  element(element) {
+    // console.log(element.tagName)
+    switch(element.tagName){
+      case 'title':
+        element.setInnerContent('Cloudflare Challenge')
+        break;
+      case 'p':
+        var choiceCount
+        if(index == 0){
+          choiceCount = [1, variant_1_count]
+        }else{
+          choiceCount = [2, variant_2_count]
+        }
+        element.setInnerContent(`This is variant ${choiceCount[0]} which has appeared: ${choiceCount[1]} time(s).`)
+        break;
+      case 'a':
+        element.setInnerContent("Visit my Linkedin Page!")
+        .setAttribute('href', 'https://www.linkedin.com/in/matt-sanford-229649173/')
+        break;
+      default:
+        //do nothing otherwise
+        break;
+    }
+  }
 }
